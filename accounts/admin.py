750e132e44e8778e354aefb2f1ccd114642194ba
t.nakamura <t.nakamura.jwoa@gmail.com>
2021-12-01 14:21:55 +0900

    11.07 日記の追加

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CustomUser
 
-from .models import Diary
 
-admin.site.register(Diary)
+admin.site.register(CustomUser)
--- private_diary/urls.py
+++ private_diary/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import static
+from . import settings_common, settings_dev
 
 urlpatterns = [
+    # path('admin/', admin.site.urls),
     path('admin/', admin.site.urls),
     path('', include('diary.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
+
+#開発serverでメディアを配信できるようにする設定
+urlpatterns += static(settings_common.MEDIA_URL, document_root=settings_dev.MEDIA_ROOT)
