7ff7551a3e26e8995fa8da0102a3d37cc3659fc8
t.nakamura <t.nakamura.jwoa@gmail.com>
2021-12-07 15:37:15 +0900

    createページの作成

--- diary/views.py
+++ diary/views.py
@@ -2,7 +2,7 @@ import logging
 
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import InquiryForm
+from .forms import InquiryForm, DiaryCreateForm
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Diary
@@ -34,3 +34,22 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
         return diaries
+
+
+
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request, '日記を作成しました。')
+        return super().form_invalid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "日記の作成に失敗しました。")
+        return super().form_invalid(form)
