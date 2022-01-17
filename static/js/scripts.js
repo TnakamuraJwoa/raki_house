30d0d96f19b09ad37449ccb8e4e461483da20ef2
t.nakamura <t.nakamura.jwoa@gmail.com>
2022-01-17 11:39:20 +0900

    jqueryの導入

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,3 +5,12 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+
+$(function(){
+  $(".btn1").on("click", function(){
+    //listBの高さを取得
+    alert("アラートを表示します。")
+  });
+});
